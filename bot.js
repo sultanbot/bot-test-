cb9d629ac3ff4457f46820837b1dd3282a22db3f
sultanbot <46088354+sultanbot@users.noreply.github.com>
2018-12-23 19:58:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(NTI2MjQ1MzcxNTM4ODMzNDA5.DwFJXw.9IwJXGfmPiZEEWC649OLUzJT-EM);
+client.login('NTI2MjQ1MzcxNTM4ODMzNDA5.DwFQQQ.Gltm9qSRKryGVipBpf4om_rwQ2M');
